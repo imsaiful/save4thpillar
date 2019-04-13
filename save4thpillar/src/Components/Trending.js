af30066bc1e5c035768c529db8359b6dffcd26d8
imsaiful <saifulcseng@gmail.com>
2019-04-13 20:30:45 +0530

    add the chart

--- save4thpillar/src/Components/Trending.js
+++ save4thpillar/src/Components/Trending.js
@@ -1,15 +1,31 @@
 import React from 'react';
-import { promised } from 'q';
-
+import {Bar} from 'react-chartjs-2';
 const Trending = (props) => {
-    console.log(props.data);
- 
+   
     
+    const data= {
+        labels:props.keyword,
+        datasets: [{
+        label: "Trending Keyword",
+        data: props.frequency,
+        backgroundColor:['#64B5F6','#E3F2FD','#F06292','#CDDC39','#EA80FC','#FF8A65','#81C784',
+        '#64B5F6','#E3F2FD','#F06292','#CDDC39','#EA80FC','#FF8A65','#81C784',
+        '#64B5F6','#E3F2FD','#F06292'
+        ],
+
+        borderWidth:3,
+        borderColor:'#777',
+        hoverBorderWidth:6,
+        hoverBorderColor:'#000'
+       
+        }]
+    }
     return (
         <div>
-            {props.data.map(({ db_keyword, db_frequency }) => <li>Keyword: {db_keyword}, Frequency: {db_frequency}</li>)}
-
-         
+            {/* {props.data.map(({ db_keyword, db_frequency }) => <li>Keyword: {db_keyword}, Frequency: {db_frequency}</li>)} */}
+            < Bar data={data} />
+           
+            
         </div>
     ) 
 }
--- save4thpillar/src/Container/TrendingListView.js
+++ save4thpillar/src/Container/TrendingListView.js
@@ -5,21 +5,32 @@ import axios from 'axios';
 
 class TrendingList extends React.Component{
   state = {
-    keywords:[]
+    data:[],
+    frequency: [],
+    keyword: []
   }
 
   componentDidMount(){
     axios.get('http://127.0.0.1:8000/api/trending')
-    .then(res=>{
+    .then(res => {
+      const data = res.data;
+      console.log(data);
+      
+      data.map(d => {
+        this.state.keyword.push(d.db_keyword)
+        this.state.frequency.push(d.db_frequency)
+        
+        
+      })
       this.setState({
-        keywords:res.data
-      });
-      console.log(res.data);
+        data: res.data
+              });
+      console.log('state data goes here:   '+ this.state.data[0].db_keyword);
     }) 
   }
   render(){
       return (
-        <Trending data={this.state.keywords}/>
+        <Trending keyword={this.state.keyword} frequency={this.state.frequency}/>
       )
   }
 }
