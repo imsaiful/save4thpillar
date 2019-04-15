9b71e1fb558f3f11ee1a7710f6d962653d3c9aed
imsaiful <saifulcseng@gmail.com>
2019-04-15 19:52:05 +0530

    remove enhance problem

--- save4thpillar/src/index.js
+++ save4thpillar/src/index.js
@@ -5,21 +5,17 @@ import reducer from './Store/reducers/auth';
 import {createStore,compose , applyMiddleware} from 'redux';
 import thunk from 'redux-thunk';
 import {Provider} from 'react-redux'
-const composeEnhances = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || composeEnhances
 
-const store= createStore(reducer,composeEnhances(
+const composeEnhances = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+
+const store = createStore(reducer, composeEnhances(
     applyMiddleware(thunk)
 ));
 
 const app = (
-        <Provider store={store}>
-             <App/> 
-        </Provider>
-       
-
+    <Provider store={store}>
+        <App />
+    </Provider>
 )
 
-ReactDOM.render(
-    app,
-    document.querySelector('#root')
-)
+ReactDOM.render(app, document.getElementById('root'));
