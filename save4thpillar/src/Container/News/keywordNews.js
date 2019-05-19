1d79aaf1c098f90cc957d462b7388a0e88441894
imsaiful <saifulcseng@gmail.com>
2019-05-20 02:56:32 +0530

    add all source fetch content

--- save4thpillar/src/Container/News/keywordNews.js
+++ save4thpillar/src/Container/News/keywordNews.js
@@ -1,29 +1,42 @@
 import React, { Component } from 'react'
 import axios from 'axios'
-import Show from './Show'
+import NewsSource from './NewsSource'
 class keywordNews extends Component {
     state = {
         data:[]
       }
-      componentDidMount(){
+      async componentDidMount(){
           const keyword=localStorage.getItem('keyword');
           const url=`http://ec2-13-233-199-251.ap-south-1.compute.amazonaws.com/api/keyword/${keyword}`;
           console.log(url);
-          axios.get(url)
-          .then(res => {
-            this.setState({data:res.data.Zeenews
+          axios.get(url).then(res => {
+            let result = Object.keys(res.data).map(function(key) {
+                return [key, res.data[key]];
             });
-            console.log(res.data);
+            this.setState({
+                data : result
+            });
         });
           
       }
   render() {
+    if (this.state && this.state.data) {
+      var allNewsCard =
+        this.state &&
+        this.state.data &&
+        this.state.data.map((item, index) => {
+          return (
+              <NewsSource data={item} key={index} />
+          );
+        });
+    } else return null;
+
     return (
-      <div>
-        <h1>{localStorage.getItem('keyword')}</h1>
-        <Show data={this.state.data} /> 
+      <div className="App">
+        
+        {allNewsCard}
       </div>
-    )
+    );
   }
 }
 
