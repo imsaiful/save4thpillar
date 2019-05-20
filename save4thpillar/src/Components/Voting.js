501d066e86626c3909c05d15c08512906db0b85c
imsaiful <saifulcseng@gmail.com>
2019-05-20 17:42:09 +0530

    add card to voting

--- save4thpillar/src/Components/Voting.js
+++ save4thpillar/src/Components/Voting.js
@@ -27,22 +27,36 @@ class Voting extends Component{
     }
     
     render(){
-    return (
-        
+    return (    
         <div>
          {this.props.data.map(({name,info,total_star,total_user,website,image }, index) => 
             <div key={index}>
             <div>
             <div className="thumbnail">
-          <div className="row">
-              <div className="col-md-8">
-                  <table className="table ">
-                      <tbody>
-                        <tr><td className="td-voting"><h5><b>Name</b></h5></td><td><h5>{name}</h5></td></tr>
-                        <tr><td className="td-voting"><h5><b>Info</b></h5></td><td><h5>{info}</h5></td></tr>
-                        <tr><td className="td-voting"><h5><b>Anchor:</b></h5></td><td><h5>{total_star/total_user}</h5></td></tr>
-                        <tr><td className="td-voting"><h5><b>Rating</b></h5></td><td><h5>{total_star/total_user}</h5></td></tr>
-                        <tr><td className="td-voting"><h5><b>You rate</b></h5></td><td><h5> <StarRatingComponent 
+            <div className="row">
+                <div className="col-md-3">
+                    <img className="img-responsive center" src={image} alt="backmyitem" style={{ width:"100%",height:"100%",margin:"0 auto"}}></img><br></br>
+                </div>
+                <div className="col-md-9">
+                    <div className="row">
+                        <div className="col-md-4">Name</div>
+                        <div className="col-md-8">{name}</div>
+                    </div>
+                    <div className="row">
+                        <div className="col-md-4">Anchors:</div>
+                        <div className="col-md-8">{info}</div>
+                    </div>
+                    <div className="row">
+                        <div className="col-md-4">Rating</div>
+                        <div className="col-md-8">{total_star/total_user}</div>
+                    </div>
+                    <div className="row">
+                        <div className="col-md-4">Website</div>
+                        <div className="col-md-8"><a href={website}>{website}</a></div>
+                    </div>
+                    <div className="row">
+                        <div className="col-md-4">You Rate</div>
+                        <div className="col-md-8"><StarRatingComponent 
                                     name="rate" 
                                     starCount={10}
                                     value={total_star/total_user}
@@ -52,74 +66,15 @@ class Voting extends Component{
                                         </span>
                                       )}
                                     onStarClick={this.onStarClick.bind(this)}
-                                />  </h5></td></tr>
-                        <tr><td className="td-voting"><h5><b>Websites</b></h5></td><td><h5>              <Link style={{ fontSize:"10px" }}>{website}</Link></h5></td></tr>
-
-                     </tbody>
-                  </table>
-              </div>
-              <div className="col-md-4">
-                  <img className="img-responsive center" src={image} alt="backmyitem" style={{ width:"100%",height:"100%",margin:"0 auto"}}></img><br></br>
-                                                                                                                        </div>
-          </div>
-         
-             
-
-         
-      </div>
-
-
+                                /> </div>
+                        </div>
+                    </div>
+                    
+                </div>
+             </div>
             </div>
-                {/* <div className="container">
-  <div className="row">
-    <div className="col-12 col-sm-8 col-md-6 col-lg-4">
-      <div className="card">
-        <img style={{ width:"100%",height:"200px"}}  className="card-img" src={image} alt="Bologna"/>
-       
-        <div className="card-body">
-          <h4 className="card-title">{name}</h4>
-            <table className="table table-responsive">
-                <tbody>
-                <tr>
-                    <td className="card-left-td">
-                    Total Rating
-                    </td>
-                    <td >
-                    {total_star/total_user}
-                    </td>
-                </tr>
-                <tr>
-                    <td className="card-left-td">
-                    You Rate
-                    </td>
-                    <td className="align-middle">
-                    <StarRatingComponent 
-                                    name="rate" 
-                                    starCount={10}
-                                    value={total_star/total_user}
-                                    renderStarIcon={() => (
-                                        <span style={{ fontSize: "20px" , margin: "0 auto" }}>
-                                          <i className="fas fa-star" />
-                                        </span>
-                                      )}
-                                    onStarClick={this.onStarClick.bind(this)}
-                                />  
-                    </td>
-                </tr>
-            
-                </tbody>
-            </table>
-           
-          
-        </div>
-        
-      </div>
+        </div>)}
     </div>
-  </div> */}
-
-
-            <br></br></div>)}
-        </div>
     ) 
     }
 }
