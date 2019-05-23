0d396c9554a3a100af3c0959944f55ca41ce71b5
imsaiful <saifulcseng@gmail.com>
2019-05-23 13:58:52 +0530

    add pie chart for category

---
+++ save4thpillar/src/Container/CategoryRatioView.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react'
+import CategoryRatio from '../Components/CategoryRatio'
+import axios from 'axios';
+
+
+class CategoryRatioView extends Component {
+
+
+    state = {
+        data:[],
+        value: [],
+        category: []
+    }
+
+
+  componentDidMount(){
+        axios.get('http://127.0.0.1:8000/api/category')
+        .then(res => {
+        const data = res.data;
+   
+
+        data.map(d => {
+            this.state.category.push(d.category)
+            this.state.value.push(d.value);
+        })
+        console.log(this.state.value);
+        this.setState({
+            data: res.data
+                });
+        }) 
+    }
+
+
+  
+    
+    render() {
+        return (
+            <div>
+                <CategoryRatio category={this.state.category} value={this.state.value}/>
+            </div>
+        )
+    }
+}
+
+
+export default CategoryRatioView
--- save4thpillar/src/Container/StatsView.js
+++ save4thpillar/src/Container/StatsView.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import TrendingListView from './TrendingListView';
+import CategoryRatioView from './CategoryRatioView';
 
  const StatsView = (props) => {
     
     return (
       <div>
 
-        <h1>Stats</h1>
+        <h1>Category Ratio</h1>
+         <CategoryRatioView />
+         <h1>Trending Keyword</h1>
          <TrendingListView />
        </div>
     )
